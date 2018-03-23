a27bb58dacb8634b4435a6d342c3c5deb2072597
Raymon den Hartogh <raydenhartogh@gmail.com>
2018-03-23 12:52:02 +0100

    first commit

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameControl : MonoBehaviour {
+
+	public static GameControl instance;
+	public GameObject gameOverText;
+	public Text scoreText;
+	public bool gameOver = false;
+	public float scrollSpeed = -1.5f;
+	private int score = 0;
+
+	// Use this for initialization
+	void Awake () 
+	{
+        //If we don't currently have a game control...
+        if (instance == null)
+            //...set this one to be it...
+            instance = this;
+        //...otherwise...
+        else if(instance != this)
+            //...destroy this one because it is a duplicate.
+            Destroy (gameObject);
+    }
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (gameOver == true && Input.GetMouseButtonDown (0))
+		{
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		}
+	}
+
+	public void BirdScored()
+	{
+		if (gameOver)
+		{
+			return;
+		}
+		score++;
+		scoreText.text = "Score " + score.ToString ();
+			
+	}
+
+	public void BirdDied()
+	{
+		gameOverText.SetActive (true);
+		gameOver = true;
+	}
+}
---
+++ Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour {
+
+	private BoxCollider2D groundCollider;
+	private float groundHorizontallength;
+
+	// Use this for initialization
+	private void Awake () 
+	{
+		groundCollider = GetComponent<BoxCollider2D> ();
+		groundHorizontallength = groundCollider.size.x;
+			
+	}
+	
+	// Update is called once per frame
+	void Update () 
+
+	{
+		if (transform.position.x < -groundHorizontallength)
+		{
+			RepositionBackground ();
+		}
+
+	}
+
+	private void RepositionBackground()
+	{
+		Vector2 groundOffSet = new Vector2(groundHorizontallength * 2f, 0);
+
+		transform.position = (Vector2)transform.position + groundOffSet;
+	}
+	
+}
